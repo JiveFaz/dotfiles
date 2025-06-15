a76b47d30b445191955693682aa846ee1ccb0393
JiveFaz <145421489+JiveFaz@users.noreply.github.com>
2025-06-15 12:28:49 -0400

    Add files via upload

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -4,3 +4,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>fv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>ft", ":NvimTreeToggle<CR>")
+
+-- Writing Toggle
+vim.keymap.set("n", "<leader>wr", ":set spell<CR>:set linebreak<CR>")
